6322a02503365bf85fe3c9c77b84c77dbf504a86
motoshige322 <motoshige100@gmail.com>
2025-04-16 19:37:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+products = ['apple', 'potato', 'garlic']
+# products.insert(3, 'banana')  # метод добавлния по индексу
+# products.append('tomato')  # метод добавлния в самый конец
+# index = products.index('potato')
+# print(index)
+# products.pop(index)
+# print(products)
+
+
+#  Функция Sorted сортирует временно
+# print(sorted(products))
+# nums = input()
+# nums_split = nums.split(', ')
+# print(nums_split)
+# new = ' '.join(nums_split)
+# print(new)
+# we = input()
+# new1 = '+'.join(we)
+# print(new1)
+
+# products.insert(2, 4)
+# products.append()
+# products.remove()
+# products.index('weer')
+# products.pop(index)
+
+
+# Упражнение 110. Порядок сортировки
+# new = []
+# ter = int(input(' '))
+# new.append(ter)
+# while ter != 0:
+#     ter = int(input(' '))
+#     if ter != 0:
+#         new.append(ter)
+#
+# sorted_new = sorted(new)
+# for i in sorted_new:
+#     print(i)
+
+
+
+
+# Упражнение 111. Обратный порядок
+# lst = []
+# inp = int(input())
+# lst.append(inp)
+# while inp !=0 :
+#     inp = int(input())
+#     if inp !=0:
+#         lst.append(inp)
+#
+# print(lst)
+# sorted_lst = sorted(lst, reverse=True)
+# print(sorted_lst)
+
+
+# Упражнение 114. Отрицательные, положительные и нули
+# lst = []
+# inp = input()
+# while inp != ' ':
+#     inp = input()
+#     if inp != ' ':
+#         lst.append(inp)
+#
+# num_lst = [float(i) for i in lst]
+# print(num_lst)
+# order = []
+# min = 0
+# for i in num_lst:
+#     order.append(i)
+#
+# for i in sorted(order):
+#     print(i)
+
+
+
+
+
+
